d3ac7c1da558e478af05a814cda452b4e6b12410
Nishant Choudhary <cs.nishantchoudhary@gmail.com>
2024-10-13 18:05:28 +0530

    package json me changes kiye

--- Backend/index.js
+++ Backend/index.js
@@ -7,36 +7,56 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-import path from "path";
 
-dotenv.config();
+dotenv.config({});
 
-connectDB();
-const PORT = process.env.PORT || 3000;
 const app = express();
 
-const _dirname = path.resolve();
-// middleware
+// MIDDLEWARE
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
 
+const corsOptions = {
+  origin: [
+    "https://job-hunt-fullstack.onrender.com", // Backend URL (if needed)
+    "http://localhost:5173", // Local development frontend
+    "https://jobhunt7.netlify.app", // Deployed frontend on Netlify
+  ],
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
 app.use(cors(corsOptions));
 
-// api's
+// const corsOptions = {
+//   origin: "http://localhost:5173",
+//   credentials: true,
+// };
+// app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+// API's
+
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-// app.use(express.static(path.join(_dirname, "/frontend/dist")));
-// app.get("*", (_, res) => {
-//   res.sendfile(path.resolve(_dirname, "frontend", "dist", "index.html"));
-// });
+// "https://localhost:3000/api/v1/user/register"
+// "https://localhost:3000/api/v1/user/login"
+// "https://localhost:3000/api/v1/user/profile/update"
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running at port ${PORT}`);
+});
+
+// TESTING API
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "I'm coming from backend",
+    success: true,
+  });
 });
