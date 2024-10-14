09ff799dc8402c5139d81767d81f3f7c644454d1
Nishant Choudhary <cs.nishantchoudhary@gmail.com>
2024-10-14 23:58:43 +0530

    base Url

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,10 +1,14 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const USER_API_END_POINT =
+  "https://job-portal-website-6.onrender.com/api/v1/user";
+export const JOB_API_END_POINT =
+  "https://job-portal-website-6.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+  "https://job-portal-website-6.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://job-portal-website-6.onrender.com/api/v1/company";
 
-// ------------------- OLD URL ------------------------------ 
+export const BASE_URL = "https://job-portal-website-6.onrender.com";
+// ------------------- OLD URL ------------------------------
 // export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
 // export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
 // export const APPLICATION_API_END_POINT =
